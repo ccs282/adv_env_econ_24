db5992c505bbaa24d96fcafd66358aa6ad564274
jgrunau <73277344+ccs282@users.noreply.github.com>
2024-04-21 17:02:32 +0200

    upload top of solutions

---
+++ empirical/src/zaklan_replication.r
@@ -0,0 +1,10 @@
+# Load packages ---------------------------------------------------------------
+library(tidyverse)
+library(magrittr)
+library(glue)
+library(here)
+library(MatchIt)
+
+
+# Set reference point for relative paths --------------------------------------
+here()
